c8e927d0be7cc9b8c2339d4903d6fa84f337ca06
Margaret Vanessa Molina Urrutia <mvmolina@prismamp.com>
2023-04-14 20:08:43 -0300

    Vista del Video youtube

--- public/js/app.js
+++
@@ -1,5 +0,0 @@
-function loadfileVideo(input_id, video_id) {
-  var fileInput = document.getElementById(input_id);
-  var fileUrl = window.URL.createObjectURL(fileInput.files[0]);
-  document.getElementById(video_id).src = fileUrl;
-}
