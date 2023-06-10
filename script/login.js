abb5901751a156025f9f60fc39abfe2b6dd8d33e
MargaretVanessa <margaretvmolinau@gmail.com>
2023-06-10 16:23:39 -0300

    cambio logo

--- script/login.js
+++ script/login.js
@@ -4,39 +4,36 @@ const btnDocente = document.querySelector('#btn-docente');
 const btnEstudiante = document.querySelector('#btn-estudiante');
 
 correo.focus();
-form.addEventListener('click', function(e) {
-  e.preventDefault();
-  correo.focus();
-  const correoValue = correo.value;
+form.addEventListener('click', function (e) {
+    e.preventDefault();
+    correo.focus();
+    const correoValue = correo.value;
+    // Validar correo electrónico
+    if (! isValidEmail(correoValue)) {
+        Swal.fire('Por favor, ingrese un correo electrónico válido.')
+        return;
+    }
+    // Validar ingreso por perfil
+    if (e.target === btnEstudiante && correoValue !== "estudiante@gmail.com") {
+        Swal.fire('Lo sentimos, el correo electrónico ingresado no corresponde a un perfil de Estudiante.');
+        return;
+    }
 
-  // Validar correo electrónico
-  if (!isValidEmail(correoValue)) {
-    //alert('Por favor, ingrese un correo electrónico válido.'); //
-    Swal.fire('Por favor, ingrese un correo electrónico válido.')
-    return;
-  }
+    if (e.target === btnDocente && correoValue !== "docente@gmail.com") {
+        Swal.fire('Lo sentimos, el correo electrónico ingresado no corresponde a un perfil de Docente.');
+        return;
+    }
 
-  if (e.target === btnEstudiante && correoValue !== "estudiante@gmail.com") {
-    Swal.fire('Lo sentimos, el correo electrónico ingresado no corresponde a un perfil de Estudiante.');
-    return;
-  }
-
-  if (e.target === btnDocente && correoValue !== "docente@gmail.com") {
-    Swal.fire('Lo sentimos, el correo electrónico ingresado no corresponde a un perfil de Docente.');
-    return;
-  }
-
-  // Redireccionar a la página correspondiente
-  if (e.target === btnDocente) {
-    window.location.href = 'page/perfilDocente.html';
-  } else if (e.target === btnEstudiante) {
-    window.location.href = 'page/proyectos.html';
-  }
+    // Redireccionar a la página correspondiente
+    if (e.target === btnDocente) {
+        window.location.href = 'page/perfilDocente.html';
+    } else if (e.target === btnEstudiante) {
+        window.location.href = 'page/proyectos.html';
+    }
 });
 
-function isValidEmail(email) {
-
+function isValidEmail(email) { 
   // Expresión regular para validar correo electrónico
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
 }
